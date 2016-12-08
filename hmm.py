54996fe5356bb62259fc1d400cd4cea7e3dc9ef0
James Stoyell <jms852@cornell.edu>
2016-12-08 13:35:33 -0500

    add support for optional clusters

--- hmm.py
+++ hmm.py
@@ -15,7 +15,8 @@ NEUTRAL_TAGS = set(['NEUTRAL', 'THOUGHTFUL'])
 NEGATIVE_TAGS = set(['SAD', 'ANGRY'])
 
 class HiddenMarkovModel(object):
-    def __init__(self, train_data):
+    def __init__(self, train_data, using_clusters=True):
+        self.using_clusters=using_clusters
         self.tag_pair_counts = Counter()
         self.tag_counts = Counter()
         self.word_tag_counts = Counter()
@@ -23,7 +24,10 @@ class HiddenMarkovModel(object):
         for tweet in tqdm(train_data):
             self.tag_counts.update(x.tag for x in tweet)
             self.tag_pair_counts.update((x.tag, y.tag) for x,y in zip(tweet, tweet[1:]))
-            self.word_tag_counts.update((clusters.get_cluster_id(x.word), x.tag) for x in tweet)
+            if self.using_clusters:
+                self.word_tag_counts.update((clusters.get_cluster_id(x.word), x.tag) for x in tweet)
+            else:
+                self.word_tag_counts.update((x.word, x.tag) for x in tweet)
         self.epsilon = (min((x[1] for x in self.word_tag_counts.iteritems()))/max(x[1] for x in self.tag_counts.iteritems())) * .1
 
 
@@ -31,14 +35,22 @@ class HiddenMarkovModel(object):
         return self.tag_pair_counts[t1, t2] / self.tag_counts[t1]
 
     def _get_word_tag_probability(self, word, tag):
-        if self.word_tag_counts[clusters.get_cluster_id(word), tag] > 0:
-            return self.word_tag_counts[clusters.get_cluster_id(word), tag] / self.tag_counts[tag]
+        if self.using_clusters:
+            if self.word_tag_counts[clusters.get_cluster_id(word), tag] > 0:
+                return self.word_tag_counts[clusters.get_cluster_id(word), tag] / self.tag_counts[tag]
+            else:
+                return self.epsilon
         else:
-            return self.epsilon
+            if self.word_tag_counts[word, tag] > 0:
+                return self.word_tag_counts[word, tag] / self.tag_counts[tag]
+            else:
+                return self.epsilon
 
     def tag_tweet(self, tweet):
         for i, t in enumerate(tweet):
-            tweet[i] = "".join(c for c in t if c in string.ascii_letters + string.digits)
+            new = "".join(c for c in t if c in string.ascii_letters + string.digits)
+            if new != "":
+                tweet[i] = new
         GraphNode = namedtuple('GraphNode', ['tag', 'prob', 'bptr'])
         graph = [[GraphNode(TAGS[0], 1.0, -1)]]
         for token in tweet[1:]: 
@@ -76,24 +88,29 @@ class HiddenMarkovModel(object):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="A tweet mood tagger CS 4701")
-    parser.add_argument("training_tweets")
+    parser.add_argument("training_tweets", help="The tagged tweets to train the model")
+    parser.add_argument("--no_clusters", help="Don't use word clusters", action="store_const", const=True, default=False)
+    parser.add_argument("--test_model", help="Test model against tagged test set", default=None)
 
     args = parser.parse_args()
     tweets = process_file(args.training_tweets)
-    h = HiddenMarkovModel(tweets)
-    while True:
-        raw_input()
-        x = tweetbot.get_tweet()
-        print x
-        x = x.split()
-        if x[0] == "RT" or x[1][0] == "@":
-            x = x[2:]
-        x = ['TWEET_START'] + x
-        tagged_tweet = h.tag_tweet(x)
-        h.sentence_tag_tweet(tagged_tweet)
-        print "EMOTION: " + tagged_tweet.get_emotion()
-        print "SENTIMENT: " + tagged_tweet.get_sentiment()
-        print "--------------------------------"
-        # Dont' print the start tag
-        for w in tagged_tweet[1:]:
-            print w.word + " : " + w.tag
+    model = HiddenMarkovModel(tweets, args.no_clusters)
+    if not args.test_model:
+        while True:
+            raw_input()
+            x = tweetbot.get_tweet()
+            print x
+            x = x.split()
+            if x[0] == "RT" or x[1][0] == "@":
+                x = x[2:]
+            x = ['TWEET_START'] + x
+            tagged_tweet = model.tag_tweet(x)
+            model.sentence_tag_tweet(tagged_tweet)
+            print "EMOTION: " + tagged_tweet.get_emotion()
+            print "SENTIMENT: " + tagged_tweet.get_sentiment()
+            print "--------------------------------"
+            # Dont' print the start tag
+            for w in tagged_tweet[1:]:
+                print w.word + " : " + w.tag
+    else:
+        pass
